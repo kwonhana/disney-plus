fdde4a1d0f125a2dade7b8a612d59f20d05e5437
kwon hana <kwon hana>
2025-12-08 12:39:53 +0900

    init: mainScreen html , scss

---
+++ src/pages/Main/components/MainScreen.tsx
@@ -0,0 +1,11 @@
+import '../scss/MainScreen.scss';
+
+const MainScreen = () => {
+  // 기본 메인 basic
+  // 영화 메인 movie
+  // 시리즈 메인 series
+  // 오리지널 메인 original
+  return <div className="MainScreen series"></div>;
+};
+
+export default MainScreen;
